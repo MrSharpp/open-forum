224f71c71c0c8f4296dcadc309f154ac2856749a
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-12-23 18:29:47 +0530

    added mantine

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
+import '@mantine/core/styles.css';
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +14,16 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+      <head>
+        <ColorSchemeScript />
+      </head>
+      </head>
+      <body >
+        <MantineProvider>
+            {children}
+        </MantineProvider>
+      </body>
     </html>
   )
 }
