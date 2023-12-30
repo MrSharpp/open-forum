bb03c8d54d545fce37d00dd0d122e4ea7b215b5f
amiralam0 <sharpprogrammer2018@gmail.com>
2023-12-30 13:08:19 +0530

    Added form to create post, added create post server action

---
+++ src/app/(application)/thread/create/action.ts
@@ -0,0 +1,9 @@
+"use server";
+
+import prisma from "@/lib-server/prisma";
+import { Prisma } from "@prisma/client";
+
+export async function createPost(formData: FormData) {
+    // TODO: Add Validations
+    // TODO: Insert Data
+}
--- src/app/(application)/thread/create/page.tsx
+++ src/app/(application)/thread/create/page.tsx
@@ -2,6 +2,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import React from "react";
 import WYSIWYG from "./components/WYSIWYG";
+import { createPost } from "./action";
 
 async function CreateThread() {
   return (
@@ -9,6 +10,7 @@ async function CreateThread() {
       <div className="p-4 rounded-sm border">
         <h2 className="font-bold text-xl mb-6">Create new Thread</h2>
 
+      <form action={createPost}>
         <div className="flex flex-col gap-4">
           <div className="grid w-full items-center gap-1.5">
             <Label htmlFor="title">Title</Label>
@@ -20,6 +22,7 @@ async function CreateThread() {
             <WYSIWYG />
           </div>
         </div>
+        </form>
       </div>
     </div>
   );
