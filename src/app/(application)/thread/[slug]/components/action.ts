c23474af85c5301e7408eee820a84d8e434cd639
MohdAhmad1 <dexk2015@gmail.com>
2024-01-01 21:03:27 +0530

    Update action.ts

--- src/app/(application)/thread/[slug]/components/action.ts
+++ src/app/(application)/thread/[slug]/components/action.ts
@@ -3,6 +3,8 @@
 import prisma from "@/lib-server/prisma";
 
 export async function toggleLikePost(replyId: string, userId: string) {
+  // TODO Later to be optimized
+
   const previousLike: any[] =
     await prisma.$queryRaw`SELECT * FROM _LikedBy WHERE _LikedBy.A = ${replyId} AND _LikedBy.B = ${userId}`;
 
