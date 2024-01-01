14c4a1af88338974c9e710c526949cab33206c6c
MohdAhmad1 <dexk2015@gmail.com>
2024-01-01 21:03:06 +0530

    toggle like post

--- src/app/(application)/thread/[slug]/components/ReplyView.tsx
+++ src/app/(application)/thread/[slug]/components/ReplyView.tsx
@@ -1,13 +1,18 @@
+"use client";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import dayjs from "@/lib/dayjs";
 import { IconHeart } from "@tabler/icons-react";
+import { toggleLikePost } from "./action";
+import { useSession } from "next-auth/react";
 
 type Props = {
   data: any;
 };
 
 function ReplyView({ data }: Props) {
+  const session = useSession();
+
   return (
     <div>
       <div className="col-span-5 flex gap-3">
@@ -34,10 +39,14 @@ function ReplyView({ data }: Props) {
           </div>
         </div>
       </div>
+
       <div>
         <span dangerouslySetInnerHTML={{ __html: data.body }} />
 
         <Button
+          onClick={() => {
+            toggleLikePost(data.id, session!.data!.user!.id as string);
+          }}
           leftIcon={<IconHeart size={14} />}
           variant={"outline"}
           className="flex gap-1.5 hover:bg-white rounded-full shadow-none items-center mt-2 hover:text-red-600 w-max"
